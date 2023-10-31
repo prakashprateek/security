8d261acfb83b857d0d17d6a1bf336a903e8b0a6f
prakashprateek <30889339+prakashprateek@users.noreply.github.com>
2023-10-31 12:02:34 +1100

    Add files via upload

---
+++ src/main/java/com/berriesoft/springsecurity/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.berriesoft.springsecurity.token;
+
+public enum TokenType {
+    BEARER
+}
---
+++ src/test/java/com/berriesoft/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berriesoft.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
